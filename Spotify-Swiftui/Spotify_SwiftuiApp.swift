eb4cd1b4712d6e95b1d843885760108ecff4746d
Mohamed Saad <m.saad@majarra.com>
2024-08-01 12:54:13 +0300

    Initial Commit

---
+++ Spotify-Swiftui/Spotify_SwiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spotify_SwiftuiApp.swift
+//  Spotify-Swiftui
+//
+//  Created by Mohamed Saad on 01/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct Spotify_SwiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
